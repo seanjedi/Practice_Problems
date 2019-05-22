fc92e72169e4c9646cbb22746748a43b173bac56
Sean Patrick Malloy <spmalloy@ucdavis.edu>
2019-05-22 12:03:52 -0700

    5/22/2019
    asdf

---
+++ practice.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+int main(int argc, char* argv[]){
+    // cout << stepper(4);
+    string s = "babad";
+    string s2 = s.append(s);
+    cout << s2;
+}
